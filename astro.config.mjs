fe545281123aeb7e13221b59029cf92f11022e03
sr4380 <sr4380@columbia.edu>
2025-11-11 21:26:53 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://design-practices.github.io",
+  site: "https://sr4380.github.io",
   base: "astro-maplibre-template",
   integrations: [mdx(), sitemap(), tailwind()],
 });
